95658de3d4efb308eea27f5d48d3b12ea75d6bcc
Ivonaldo Micheluti Dias <116757602+ivonaldomdias@users.noreply.github.com>
2024-08-26 23:30:37 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -6,28 +6,28 @@ def calculadora(num1, num2, operacao):
     elif operacao == 3:
         return num1 * num2
     elif operacao == 4:
-        return num1 / num2 if num2 != 0 else "Erro: Divisão por zero"
+        return num1 / num2 if num2 != 0 else "Erro!"
     else:
         return 0
 
-# Soma
-resultado1 = calculadora(10, 5, 1)  
+# adição
+conta1 = calculadora(10, 6, 1)  
 # Subtração
-resultado2 = calculadora(10, 5, 2)  
+conta2 = calculadora(10, 4, 2)  
 # Multiplicação
-resultado3 = calculadora(10, 5, 3)  
+conta3 = calculadora(10, 9, 3)  
 # Divisão
-resultado4 = calculadora(10, 5, 4)  
-# Operação inválida
-resultado5 = calculadora(10, 5, 5)  
+conta4 = calculadora(10, 43, 4)  
+# Teste erro
+conta5 = calculadora(10, 11, 5)  
 
-print(resultado1)  
+print(conta1)  
 # 15
-print(resultado2)  
+print(conta2)  
 # 5
-print(resultado3)  
+print(conta3)  
 # 50
-print(resultado4)  
+print(conta4)  
 # 2.0
-print(resultado5)  
+print(conta5)  
 # 0
